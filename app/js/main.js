9f1e9c9ca5a8d0a87f4d45395d6b5d8c061f419d
Юрий Матыцин <ymatytsin@leads.team>
2020-02-19 18:02:36 +0300

    закончил секцию Impressed+Services+сделал галерею Portfolio

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+
+    $('.portfolio__gallery').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 270,
+        gutter: 10,
+        horizontalOrder: true
+    });
+
+
+    
+});
