85829fc0a0db67546e9703e4cb9e4b95e973bdca
Юрий Матыцин <ymatytsin@leads.team>
2020-02-26 17:08:38 +0300

    Testimonials+LATEST NEWS+Our Price

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,27 @@ jQuery(function($) {
 			// prevArrow: '<button type="button" class="slick-btn slick-prev"></button>'
 		});
 
+		$('.testimonials__box').slick({
+			fade: true,
+			draggable: false,
+			speed: 1000,
+			dots: true,
+			arrows: false,
+			dotsClass: 'slick-dots'
+		});
+
+		$('.latest__inner').slick({
+			fade: true,
+			// draggable: false,
+			speed: 1000,
+			dots: true,
+			arrows: false,
+			dotsClass: 'slick-latest'
+		});
+
+
+
+
     $('.portfolio__gallery').masonry({
         itemSelector: '.grid-item',
         columnWidth: 270,
